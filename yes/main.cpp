af2d7d0d298eb14ae30a17cf6b299d8b96ccdf1f
spineda2019 <spineda.wpi.alum@gmail.com>
2025-10-17 17:47:42 -0400

    naive yes

--- yes/main.cpp
+++ yes/main.cpp
@@ -1,10 +1,29 @@
-#include <print>
-#include <span>
-int main(int argc, const char **argv) {
-  std::span<const char *> args{argv, argv + argc};
+// Copyright 2025 Sebastian Pineda (spineda.wpi.alum@wpi.edu)
 
-  for (const char *str : args) {
-    std::println("Args: {}", str);
+#include <print>
+
+int main(int argc, const char **argv) {
+  if (argc > 2) {
+    std::println("Usage: yes [string]");
+    return 1;
   }
+
+  const char *to_print{"y"};
+
+  switch (argc) {
+  case 1:
+    break;
+  case 2:
+    to_print = argv[1];
+    break;
+  default:
+    std::println("Usage: yes [string]");
+    return 1;
+  }
+
+  while (true) {
+    std::println("{}", to_print);
+  }
+
   return 0;
 }
