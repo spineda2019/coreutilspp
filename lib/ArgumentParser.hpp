8962db91985f3992ff494967c49dd264ba52851c
spineda2019 <spineda.wpi.alum@gmail.com>
2025-10-25 17:18:48 -0400

    start simple

--- lib/ArgumentParser.hpp
+++ lib/ArgumentParser.hpp
@@ -3,46 +3,10 @@
 #ifndef LIB_ARGUMENTPARSER_HPP_
 #define LIB_ARGUMENTPARSER_HPP_
 
-#include <array>
 #include <cstddef>
-#include <cstdint>
-#include <span>
-#include <string_view>
 
-namespace core {
-
-namespace flags {
-struct Help {};
-struct Version {};
-}  // namespace flags
-
-enum class nargs : std::uint8_t {};
-
-template <class T, const char*... Names>
-class Argument final {};
-
-template <>
-class Argument<flags::Help> final {};
-
-template <>
-class Argument<flags::Version> final {};
-
-template <class T>
-class Argument<T> final {
-    static_assert(false, "Argument with a type must have at least one Name");
-};
-
-template <Argument... Args>
-class ArgumentParser final {
- public:
-    void ParseArgs(std::span<const char*> args) const {
-        for (std::string_view arg : args) {
-            (void)arg;
-        }
-    }
-
- private:
-};
-}  // namespace core
+namespace coreutils {
+class ArgumentParser final {};
+}  // namespace coreutils
 
 #endif  // LIB_ARGUMENTPARSER_HPP_
