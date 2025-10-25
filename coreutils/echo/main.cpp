f1bf4189965b68cc80f2388984c50a230e975bb3
spineda2019 <spineda.wpi.alum@gmail.com>
2025-10-25 17:17:21 -0400

    simplify

--- coreutils/echo/main.cpp
+++ coreutils/echo/main.cpp
@@ -6,20 +6,17 @@
 int main(int argc, const char** argv) {
     std::span<const char*> args{argv + 1, argv + argc};
 
-    switch (args.size()) {
-        case 0:
-            std::println();
-            break;
-        default:
-            const auto last{args.back()};
-            for (const char* arg : args) {
-                if (arg != last) {
-                    std::print("{} ", arg);
-                } else {
-                    std::println("{}", arg);
-                }
+    if (args.size()) {
+        const auto last{args.back()};
+        for (const char* arg : args) {
+            if (arg != last) {
+                std::print("{} ", arg);
+            } else {
+                std::println("{}", arg);
             }
-            break;
+        }
+    } else {
+        std::println();
     }
 
     return 0;
