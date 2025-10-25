e03f4f9d01fee89d23d60301bd6ba04b74ebbf96
spineda2019 <spineda.wpi.alum@gmail.com>
2025-10-25 17:02:30 -0400

    naive echo

--- coreutils/echo/main.cpp
+++ coreutils/echo/main.cpp
@@ -1,7 +1,26 @@
 // Copyright 2025 Sebastian Pineda (spineda.wpi.alum@gmail.com)
 
-int main(int argc, char** argv) {
-    (void)argc;
-    (void)argv;
+#include <print>
+#include <span>
+
+int main(int argc, const char** argv) {
+    std::span<const char*> args{argv + 1, argv + argc};
+
+    switch (args.size()) {
+        case 0:
+            std::println();
+            break;
+        default:
+            const auto last{args.back()};
+            for (const char* arg : args) {
+                if (arg != last) {
+                    std::print("{} ", arg);
+                } else {
+                    std::println("{}", arg);
+                }
+            }
+            break;
+    }
+
     return 0;
 }
