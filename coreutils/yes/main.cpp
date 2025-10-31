29ca990cce30b26fc763b9a1e0362bd68754f8bf
spineda2019 <spineda.wpi.alum@gmail.com>
2025-10-31 00:04:06 -0400

    use argparser

--- coreutils/yes/main.cpp
+++ coreutils/yes/main.cpp
@@ -1,29 +1,35 @@
 // Copyright 2025 Sebastian Pineda (spineda.wpi.alum@wpi.edu)
 
 #include <print>
-#include <ranges>
+#include <span>
+#include <string>
 #include <string_view>
+#include <vector>
 
 #include "lib/ArgumentParser.hpp"
 
-int main(int argc, const char **argv) {
+int main(int argc, const char** argv) {
     using PositionalArgs =
         coreutils::PositionalArguments<std::string_view,
                                        [](std::string_view v) { return v; }>;
     coreutils::ArgumentParser<"yes", "0.0.1", PositionalArgs> parser{argc,
                                                                      argv};
     parser.ParseArgsOrExit();
-    decltype(auto) pos_args{parser.get<PositionalArgs>().value};
-    if (argc > 2) {
-        std::println("Usage: yes [string]");
-        return 1;
-    }
 
-    std::string_view to_print{"y"};
+    const std::vector<std::string_view>& pos_args{
+        parser.get<PositionalArgs>().value};
 
-    if (pos_args.size()) {
-        // decltype(auto) joined{std::ranges::join_view{pos_args}};
-        // to_print = std::string_view{joined.begin(), joined.end()};
+    std::string to_print{};
+
+    if (std::vector<std::string_view>::size_type size{pos_args.size()}; size) {
+        for (const std::string_view& v :
+             std::span{pos_args.begin(), size - 1}) {
+            to_print.append_range(v);
+            to_print.push_back(' ');
+        }
+        to_print.append_range(pos_args.back());
+    } else {
+        to_print.push_back('y');
     }
 
     while (true) {
